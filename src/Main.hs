0dd9b39138df080d451624fe4a54ab9137a8430c
Fionn Cleary <clearyf@tcd.ie>
2015-10-17 01:10:28 +0200

    Basic parsing of XML file

--- src/Main.hs
+++ src/Main.hs
@@ -4,5 +4,9 @@ module Main where
 
 import BasePrelude
 
+import Text.XML.HaXml
+import Text.XML.HaXml.Pretty
+
 main :: IO ()
-main = putStrLn "Hello world"
+main = do xml <- readFile "examples/iec61449/minMax.xml"
+          putStrLn (show (document (xmlParse "minMax" xml)))
