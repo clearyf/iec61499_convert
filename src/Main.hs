d99a8879d440ce48e4df3b5c60a02e51ecb16a6a
Fionn Cleary <clearyf@tcd.ie>
2015-11-18 15:57:07 +0100

    Basic program working

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,24 @@ module Main where
 
 import BasePrelude hiding (orElse)
 import ParseIec61499 (readFunctionBlock, FunctionBlock)
+import FbToUppaal
+import OutputUppaal
+import Text.XML.HXT.Core
 
 test :: IO [FunctionBlock]
-test = do let path = "examples/iec61499/toggle_fb.xml"
-          readFunctionBlock path
+test = do
+    let path = "examples/iec61499/toggle_fb.xml"
+    readFunctionBlock path
+
+theStdout :: Monoid m => m
+theStdout = mempty
+
+doFile :: FilePath -> IO [XmlTree]
+doFile path = do
+    (contents:_) <- readFunctionBlock path
+    outputUppaalToFile (fbToUppaalModel contents) theStdout
 
 main :: IO ()
-main = do result <- test
-          print result
+main = do
+    arguments <- getArgs
+    traverse_ doFile arguments
