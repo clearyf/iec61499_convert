8d6ef85f549d4c12719a73e211327e6593ced194
Fionn Cleary <clearyf@tcd.ie>
2015-11-26 23:41:17 +0100

    ParseIec61499Spec: Add demux8

---
+++ test/Demux8.hs
@@ -0,0 +1,184 @@
+module Demux8 where
+
+import BasePrelude
+import ParseIec61499
+import ParseSt
+
+demux8Block :: [FunctionBlock]
+demux8Block =
+    [ FunctionBlock
+      { fbName = "Demux8"
+      , interfaceList = InterfaceList
+        { eventInputs = [ Event
+                          { eventName = "REQ"
+                          , eventComment = "Normal Execution Request"
+                          , eventVariables = ["inValue"]
+                          }]
+        , eventOutputs = [ Event
+                           { eventName = "CNF"
+                           , eventComment = "Execution Confirmation"
+                           , eventVariables = [ "outValue1"
+                                              , "outValue2"
+                                              , "outValue3"
+                                              , "outValue4"
+                                              , "outValue5"
+                                              , "outValue6"
+                                              , "outValue7"
+                                              , "outValue8"]
+                           }]
+        , inputVariables = [ Variable
+                             { variableName = "inValue"
+                             , variableType = IECInt
+                             , variableComment = "Input Value (from 1 to 8)"
+                             }]
+        , outputVariables = [ Variable
+                              { variableName = "outValue1"
+                              , variableType = IECBool
+                              , variableComment = "Output TRUE if inValue = 1"
+                              }
+                            , Variable
+                              { variableName = "outValue2"
+                              , variableType = IECBool
+                              , variableComment = "Output TRUE if inValue = 2"
+                              }
+                            , Variable
+                              { variableName = "outValue3"
+                              , variableType = IECBool
+                              , variableComment = "Output TRUE if inValue = 3"
+                              }
+                            , Variable
+                              { variableName = "outValue4"
+                              , variableType = IECBool
+                              , variableComment = "Output TRUE if inValue = 4"
+                              }
+                            , Variable
+                              { variableName = "outValue5"
+                              , variableType = IECBool
+                              , variableComment = "Output TRUE if inValue = 5"
+                              }
+                            , Variable
+                              { variableName = "outValue6"
+                              , variableType = IECBool
+                              , variableComment = "Output TRUE if inValue = 6"
+                              }
+                            , Variable
+                              { variableName = "outValue7"
+                              , variableType = IECBool
+                              , variableComment = "Output TRUE if inValue = 7"
+                              }
+                            , Variable
+                              { variableName = "outValue8"
+                              , variableType = IECBool
+                              , variableComment = "Output TRUE if inValue = 8"
+                              }]
+        }
+      , basicFb = BasicFunctionBlock
+        { bfbStates = [ ECState
+                        { ecStateName = "START"
+                        , ecStateComment = "Initial State"
+                        , ecStateActions = []
+                        }
+                      , ECState
+                        { ecStateName = "REQ"
+                        , ecStateComment = "Normal execution"
+                        , ecStateActions = [ ECAction
+                                             { ecActionAlgorithm = "REQ"
+                                             , ecActionOutput = "CNF"
+                                             }]
+                        }]
+        , bfbTransitions = [ ECTransition
+                             { ecTransitionSource = "START"
+                             , ecTransitionDestination = "REQ"
+                             , ecTransitionCondition = "REQ"
+                             }
+                           , ECTransition
+                             { ecTransitionSource = "REQ"
+                             , ecTransitionDestination = "START"
+                             , ecTransitionCondition = "1"
+                             }]
+        , bfbAlgorithms = [ ECAlgorithm
+                            { ecAlgorithmName = "REQ"
+                            , ecAlgorithmComment = "Normally executed algorithm"
+                            , ecAlgorithmStText = [ IfElse
+                                                        [ StVar "inValue"
+                                                        , StOp "="
+                                                        , StInt 1]
+                                                        [ Assignment
+                                                              "outValue1"
+                                                              [StBool True]]
+                                                        [ Assignment
+                                                              "outValue1"
+                                                              [StBool False]]
+                                                  , IfElse
+                                                        [ StVar "inValue"
+                                                        , StOp "="
+                                                        , StInt 2]
+                                                        [ Assignment
+                                                              "outValue2"
+                                                              [StBool True]]
+                                                        [ Assignment
+                                                              "outValue2"
+                                                              [StBool False]]
+                                                  , IfElse
+                                                        [ StVar "inValue"
+                                                        , StOp "="
+                                                        , StInt 3]
+                                                        [ Assignment
+                                                              "outValue3"
+                                                              [StBool True]]
+                                                        [ Assignment
+                                                              "outValue3"
+                                                              [StBool False]]
+                                                  , IfElse
+                                                        [ StVar "inValue"
+                                                        , StOp "="
+                                                        , StInt 4]
+                                                        [ Assignment
+                                                              "outValue4"
+                                                              [StBool True]]
+                                                        [ Assignment
+                                                              "outValue4"
+                                                              [StBool False]]
+                                                  , IfElse
+                                                        [ StVar "inValue"
+                                                        , StOp "="
+                                                        , StInt 5]
+                                                        [ Assignment
+                                                              "outValue5"
+                                                              [StBool True]]
+                                                        [ Assignment
+                                                              "outValue5"
+                                                              [StBool False]]
+                                                  , IfElse
+                                                        [ StVar "inValue"
+                                                        , StOp "="
+                                                        , StInt 6]
+                                                        [ Assignment
+                                                              "outValue6"
+                                                              [StBool True]]
+                                                        [ Assignment
+                                                              "outValue6"
+                                                              [StBool False]]
+                                                  , IfElse
+                                                        [ StVar "inValue"
+                                                        , StOp "="
+                                                        , StInt 7]
+                                                        [ Assignment
+                                                              "outValue7"
+                                                              [StBool True]]
+                                                        [ Assignment
+                                                              "outValue7"
+                                                              [StBool False]]
+                                                  , IfElse
+                                                        [ StVar "inValue"
+                                                        , StOp "="
+                                                        , StInt 8]
+                                                        [ Assignment
+                                                              "outValue8"
+                                                              [StBool True]]
+                                                        [ Assignment
+                                                              "outValue8"
+                                                              [StBool False]]]
+                            }]
+        }
+      }]
--- test/ParseIec61499Spec.hs
+++ test/ParseIec61499Spec.hs
@@ -5,13 +5,14 @@ import Test.Hspec (it, shouldBe, runIO)
 import Test.Hspec.Core.Spec (Spec)
 import ParseIec61499
 import ToggleFunctionBlock
+import Demux8
 
 spec :: Spec
 spec = do
-    toggleFunctionBlockFromFile <- runIO (readFunctionBlock toggleFile)
-    it
-        "Function Block"
-        (toggleFunctionBlockFromFile `shouldBe` toggleFunctionBlock)
+    checkFile toggleFunctionBlock "examples/iec61499/toggle_fb.xml"
+    checkFile demux8Block "examples/iec61499/demux8.xml"
 
-toggleFile :: FilePath
-toggleFile = "examples/iec61499/toggle_fb.xml"
+checkFile :: [FunctionBlock] -> FilePath -> Spec
+checkFile block filename = do
+    fromFile <- runIO (readFunctionBlock filename)
+    it ("Function Block: " <> filename) (fromFile `shouldBe` block)
